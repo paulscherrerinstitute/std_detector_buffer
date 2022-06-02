1b12513c698e5e6b9f6c900a428e2c9dc663ff0a
babic_a <andrej.babic@psi.ch>
2022-06-02 10:58:36 +0200

    Add tests to check Gigafrost structure sizes

---
+++ detectors/test/test_gigafrost.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "gigafrost.hpp"
+
+#define N_CACHE_LINE_BYTES 64
+
+TEST(Gigafrost, struct_size)
+{
+    EXPECT_EQ(sizeof(det_packet), BYTES_PER_PACKET);
+    EXPECT_EQ(sizeof(frame_metadata), N_CACHE_LINE_BYTES);
+}
