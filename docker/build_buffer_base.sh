2560578ff24b8a656ba2e78958e9afc295b07b4f
Andrej Babic <andrej.babic@ringlspil.com>
2022-06-10 14:42:40 +0200

    Make the buffer building script more readable

--- docker/build_buffer_base.sh
+++ docker/build_buffer_base.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NAME="std_detector_buffer_base"
+NAME="paulscherrerinstitute/std_detector_buffer_base"
 VERSION=2.1.0
 
-docker build -f buffer_base.Dockerfile -t paulscherrerinstitute/$NAME .
-docker tag paulscherrerinstitute/$NAME paulscherrerinstitute/$NAME:$VERSION
+docker build -f buffer_base.Dockerfile -t ${NAME} .
+docker tag ${NAME} ${NAME}:${VERSION}
 
 docker login
-docker push paulscherrerinstitute/$NAME:$VERSION
-docker push paulscherrerinstitute/$NAME:latest
+docker push ${NAME}:${VERSION}
+docker push ${NAME}:latest
