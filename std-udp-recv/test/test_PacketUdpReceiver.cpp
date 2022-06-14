782bf2397a46dbf46be6fd920cb89ce0bab187ee
Andrej Babic <andrej.babic@ringlspil.com>
2022-06-14 14:47:06 +0200

    Refactor PacketUdpRecv test

--- std-udp-recv/test/test_PacketUdpReceiver.cpp
+++ std-udp-recv/test/test_PacketUdpReceiver.cpp
@@ -13,22 +13,6 @@ using namespace std;
 
 TEST(PacketUdpReceiver, receive_many)
 {
-  auto n_msg_buffer = N_PACKETS_PER_FRAME;
-  JFUdpPacket recv_buffer[n_msg_buffer];
-  iovec recv_buff_ptr[n_msg_buffer];
-  struct mmsghdr msgs[n_msg_buffer];
-  struct sockaddr_in sockFrom[n_msg_buffer];
-
-  for (int i = 0; i < n_msg_buffer; i++) {
-    recv_buff_ptr[i].iov_base = (void*)&(recv_buffer[i]);
-    recv_buff_ptr[i].iov_len = sizeof(JFUdpPacket);
-
-    msgs[i].msg_hdr.msg_iov = &recv_buff_ptr[i];
-    msgs[i].msg_hdr.msg_iovlen = 1;
-    msgs[i].msg_hdr.msg_name = &sockFrom[i];
-    msgs[i].msg_hdr.msg_namelen = sizeof(sockaddr_in);
-  }
-
   uint16_t udp_port = MOCK_UDP_PORT;
 
   auto send_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -37,7 +21,7 @@ TEST(PacketUdpReceiver, receive_many)
   PacketUdpReceiver udp_receiver(udp_port, sizeof(JFUdpPacket), 2);
   udp_receiver.bind(udp_port);
 
-  JFUdpPacket send_udp_buffer;
+  JFUdpPacket send_udp_buffer = {};
 
   auto server_address = get_server_address(udp_port);
 
@@ -51,15 +35,17 @@ TEST(PacketUdpReceiver, receive_many)
 
   this_thread::sleep_for(chrono::milliseconds(10));
 
-  auto n_msgs = udp_receiver.receive_many(msgs, N_PACKETS_PER_FRAME);
+  auto n_msgs = udp_receiver.receive_many();
   ASSERT_EQ(n_msgs, 2);
 
+  const JFUdpPacket* const packet_buffer = reinterpret_cast<JFUdpPacket*>(
+      udp_receiver.get_packet_buffer());
+
   for (size_t i = 0; i < n_msgs; i++) {
-    ASSERT_EQ(msgs[i].msg_len, BYTES_PER_PACKET);
-    ASSERT_EQ(recv_buffer[i].bunchid, i);
+    ASSERT_EQ(packet_buffer[i].bunchid, i);
   }
 
-  n_msgs = udp_receiver.receive_many(msgs, N_PACKETS_PER_FRAME);
+  n_msgs = udp_receiver.receive_many();
   ASSERT_EQ(n_msgs, -1);
 
   udp_receiver.disconnect();
