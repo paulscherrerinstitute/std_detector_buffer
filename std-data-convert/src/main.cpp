6df243bbb9a78c1e7ea16229b6bf76790075021f
Maciej Patro <maciejpatro@gmail.com>
2022-06-22 08:41:41 +0200

    Ongoing implementation of main: - reading gains and pedestals introduced

--- std-data-convert/src/main.cpp
+++ std-data-convert/src/main.cpp
@@ -1,6 +1,40 @@
 #include "converter.hpp"
 
-int main()
+#include <fstream>
+
+#include <fmt/core.h>
+#include <rapidjson/istreamwrapper.h>
+#include <rapidjson/document.h>
+
+sdc::parameters decode_parameters(const rapidjson::Value& array_3d)
 {
-  return sizeof(sdc::Converter);
+  sdc::parameters params;
+  for (auto i = 0u; i < params.size(); i++) {
+    auto& params_line = params[i];
+    for (const auto& array_1d : array_3d[i].GetArray())
+      for (const auto& value : array_1d.GetArray())
+        params_line.push_back(value.GetFloat()); // currently these are saved as doubles
+  }
+  return params;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    fmt::print("Usage: std_data_convert [gains_and_pedestals_json]\n\n"
+               "\tgains_and_pedestals_json: Json file containing gains .\n");
+    exit(-1);
+  }
+
+  std::ifstream ifs(argv[1]);
+  rapidjson::IStreamWrapper isw(ifs);
+  rapidjson::Document json_data;
+  json_data.ParseStream(isw);
+
+  sdc::parameters gains = decode_parameters(json_data["gains"]);
+  sdc::parameters pedestals = decode_parameters(json_data["pedestals"]);
+
+  sdc::Converter converter(gains, pedestals);
+
+  return gains.size() + pedestals.size();
 }
