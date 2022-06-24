84d8f629a6fe56575c3ce025c73c061faf74ccce
Maciej Patro <maciejpatro@gmail.com>
2022-06-24 08:16:43 +0200

    [converter] Read gains and pedestals from separate json file

--- std-data-convert/src/main.cpp
+++ std-data-convert/src/main.cpp
@@ -20,23 +20,29 @@ sdc::parameters decode_parameters(const rapidjson::Value& array_3d)
   return params;
 }
 
+std::tuple<sdc::parameters, sdc::parameters> read_gains_and_pedestals(std::string_view filename)
+{
+  std::ifstream ifs(filename.data());
+  rapidjson::IStreamWrapper isw(ifs);
+  rapidjson::Document json_data;
+  json_data.ParseStream(isw);
+
+  return {decode_parameters(json_data["gains"]), decode_parameters(json_data["pedestals"])};
+}
+
 int main(int argc, char* argv[])
 {
-  if (argc != 3) {
-    fmt::print("Usage: std_data_convert [detector_json_filename] [module_id]\n\n"
+  if (argc != 4) {
+    fmt::print("Usage: std_data_convert [detector_json_filename] [gains_and_pedestals_json] "
+               "[module_id]\n\n"
                "\tdetector_json_filename: detector config file path.\n"
+               "\tgains_and_pedestals_json: gains and pedestals json path.\n"
                "\tmodule_id: id of the module for this process.\n");
     exit(-1);
   }
 
   const auto config = buffer_utils::read_json_config(std::string(argv[1]));
-  std::ifstream ifs(argv[1]);
-  rapidjson::IStreamWrapper isw(ifs);
-  rapidjson::Document json_data;
-  json_data.ParseStream(isw);
-
-  sdc::parameters gains = decode_parameters(json_data["gains"]);
-  sdc::parameters pedestals = decode_parameters(json_data["pedestals"]);
+  auto [gains, pedestals] = read_gains_and_pedestals(argv[2]);
 
   sdc::Converter converter(gains, pedestals);
 
