828bb6ec58f47c1e10ca3f03f4131bbcd272df82
Andrej Babic <andrej.babic@ringlspil.com>
2022-06-24 16:31:00 +0200

    Rename frame_id into frame_index for consistency

--- detectors/gigafrost.hpp
+++ detectors/gigafrost.hpp
@@ -16,8 +16,7 @@ struct GFUdpPacket
 {
   uint8_t protocol_id;                   // Fixed to 0xCB
   uint8_t quadrant_row_length_in_blocks; // Length of quadrant row in 12 pixel blocks -> 24 .. 1008
-  uint8_t
-      quadrant_rows; // Number of rows in each quadrant. quadrant_rows[0] == SWAP bit. -> 2 .. 1008
+  uint8_t quadrant_rows; // N rows in each quadrant. quadrant_rows[0] == SWAP bit. -> 2 .. 1008
   // bit[6-7] == quadrant_id 	GF_NE=3 GF_NW=2 GF_SE=1 GF_SW=0
   // bit[5] == link_id, [0, 1]
   // bit[2-4] == corr_mode,
@@ -26,7 +25,7 @@ struct GFUdpPacket
   uint32_t scan_id; // Unique ID of the scan.
   // Up to here everything is static per socket and scan.
 
-  uint32_t frame_id;            // Number of the frame in the scan.
+  uint32_t frame_index;         // Number of the frame in the scan.
   uint16_t image_status_flags;  // bit[15] = do not store, rest is debug bits.
   uint16_t packet_starting_row; // Starting row of the current UDP packet.
   uint64_t image_timing;        // byte[0-4] = timestamp, bytes[3-7] = exposure_time
