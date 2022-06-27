f47278ec2075bdf1620184bbc062d45d0dc5f6a6
Andrej Babic <andrej.babic@ringlspil.com>
2022-06-27 10:54:30 +0200

    Add module size to struct

--- std-udp-recv/src/gigafrost/main.cpp
+++ std-udp-recv/src/gigafrost/main.cpp
@@ -28,71 +28,79 @@ int main(int argc, char* argv[])
 
     exit(-1);
   }
-//  const auto config = read_json_config(string(argv[1]));
+  const auto config = read_json_config(string(argv[1]));
   const uint16_t module_id = stoi(argv[2]);
-  cout << module_id << endl;
-//
-//  auto ctx = zmq_ctx_new();
-//  cb::Sender sender{{config.detector_name + std::to_string(module_id),
-//                     BYTES_PER_PACKET - DATA_BYTES_PER_PACKET,
-//                     DATA_BYTES_PER_PACKET * N_PACKETS_PER_FRAME, RAM_BUFFER_N_SLOTS},
-//                    ctx};
-//
-//  PacketUdpReceiver receiver(config.start_udp_port + module_id, sizeof(GFUdpPacket),
-//                             N_PACKETS_PER_FRAME);
-//  FrameStats stats(config.detector_name, module_id, STATS_TIME);
-//
-//  const GFUdpPacket* const packet_buffer =
-//      reinterpret_cast<GFUdpPacket*>(receiver.get_packet_buffer());
-//  GFFrame meta = {};
-//  meta.frame_index = INVALID_FRAME_INDEX;
-//
-//  char* frame_buffer = new char[MODULE_N_BYTES];
-//
-//  while (true) {
-//    // Load n_packets into the packet_buffer.
-//    const auto n_packets = receiver.receive_many();
-//
-//    for (int i_packet = 0; i_packet < n_packets; i_packet++) {
-//      const auto& packet = packet_buffer[i_packet];
-//
-//      // Packet belongs to the frame we are currently processing.
-//      if (meta.frame_index == packet.frame_index) {
-//        // Accumulate packets data into the frame buffer.
-//        const size_t frame_buffer_offset = packet.packetnum * DATA_BYTES_PER_PACKET;
-//        memcpy(frame_buffer + frame_buffer_offset, packet.data, DATA_BYTES_PER_PACKET);
-//        meta.n_missing_packets -= 1;
-//
-//        // Copy frame_buffer to ram_buffer and send pulse_id over zmq if last packet in frame.
-//        // TODO: Check comparison between size_t and uint32_t
-//        if (packet.packetnum == N_PACKETS_PER_FRAME - 1) {
-//          sender.send(meta.frame_index, reinterpret_cast<char*>(&meta), frame_buffer);
-//          stats.record_stats(meta.n_missing_packets);
-//          // Invalidate the current buffer - we already send data out for this one.
-//          meta.frame_index = INVALID_FRAME_INDEX;
-//        }
-//      }
-//      else {
-//        // The buffer was not flushed because the last packet from the previous frame was missing.
-//        if (meta.frame_index != INVALID_FRAME_INDEX) {
-//          sender.send(meta.frame_index, reinterpret_cast<char*>(&meta), frame_buffer);
-//          stats.record_stats(meta.n_missing_packets);
-//        }
-//
-//        // Initialize new frame metadata from first seen packet.
-//        meta.frame_index = static_cast<uint64_t>(packet.frame_index);
-//        meta.n_missing_packets = N_PACKETS_PER_FRAME;
-//        meta.scan_id = packet.scan_id;
-//        meta.size_x = ;
-//        meta.size_y = ;
-//
-//        // Accumulate packets data into the frame buffer.
-//        const size_t frame_buffer_offset = packet.packetnum * DATA_BYTES_PER_PACKET;
-//        memcpy(frame_buffer + frame_buffer_offset, packet.data, DATA_BYTES_PER_PACKET);
-//        meta.n_missing_packets -= 1;
-//      }
-//    }
-//  }
-//
-//  delete[] frame_buffer;
+
+  // TODO: Unify naming for bytes and pixels -> module_size tells you nothing.
+  const uint32_t module_size_x = config.image_width / 2;
+  const uint32_t module_size_y = config.image_height / 2;
+
+  auto ctx = zmq_ctx_new();
+  cb::Sender sender{{config.detector_name + std::to_string(module_id),
+                     BYTES_PER_PACKET - DATA_BYTES_PER_PACKET,
+                     DATA_BYTES_PER_PACKET * N_PACKETS_PER_FRAME, RAM_BUFFER_N_SLOTS},
+                    ctx};
+
+  PacketUdpReceiver receiver(config.start_udp_port + module_id, sizeof(GFUdpPacket),
+                             N_PACKETS_PER_FRAME);
+  FrameStats stats(config.detector_name, module_id, STATS_TIME);
+
+  const GFUdpPacket* const packet_buffer =
+      reinterpret_cast<GFUdpPacket*>(receiver.get_packet_buffer());
+  GFFrame meta = {};
+  meta.frame_index = INVALID_FRAME_INDEX;
+
+  char* frame_buffer = new char[MODULE_N_BYTES];
+
+  while (true) {
+    // Load n_packets into the packet_buffer.
+    const auto n_packets = receiver.receive_many();
+
+    for (int i_packet = 0; i_packet < n_packets; i_packet++) {
+      const auto& packet = packet_buffer[i_packet];
+
+      // Packet belongs to the frame we are currently processing.
+      if (meta.frame_index == packet.frame_index) {
+        // Accumulate packets data into the frame buffer.
+        const size_t frame_buffer_offset = packet.packetnum * DATA_BYTES_PER_PACKET;
+        memcpy(frame_buffer + frame_buffer_offset, packet.data, DATA_BYTES_PER_PACKET);
+        meta.n_missing_packets -= 1;
+
+        // Copy frame_buffer to ram_buffer and send pulse_id over zmq if last packet in frame.
+        // TODO: Check comparison between size_t and uint32_t
+        if (packet.packetnum == N_PACKETS_PER_FRAME - 1) {
+          sender.send(meta.frame_index, reinterpret_cast<char*>(&meta), frame_buffer);
+          stats.record_stats(meta.n_missing_packets);
+          // Invalidate the current buffer - we already send data out for this one.
+          meta.frame_index = INVALID_FRAME_INDEX;
+        }
+      }
+      else {
+        // The buffer was not flushed because the last packet from the previous frame was missing.
+        if (meta.frame_index != INVALID_FRAME_INDEX) {
+          sender.send(meta.frame_index, reinterpret_cast<char*>(&meta), frame_buffer);
+          stats.record_stats(meta.n_missing_packets);
+        }
+
+        // Initialize new frame metadata from first seen packet.
+        meta.frame_index = static_cast<uint64_t>(packet.frame_index);
+        meta.n_missing_packets = N_PACKETS_PER_FRAME;
+
+        meta.scan_id = packet.scan_id;
+        meta.size_x = module_size_x;
+        meta.size_y = module_size_y;
+        meta.sync_time = packet.sync_time;
+        meta.scan_time = packet.scan_time;
+        meta.frame_timestamp = 0;
+        meta.exposure_time = 0;
+
+        // Accumulate packets data into the frame buffer.
+        const size_t frame_buffer_offset = packet.packetnum * DATA_BYTES_PER_PACKET;
+        memcpy(frame_buffer + frame_buffer_offset, packet.data, DATA_BYTES_PER_PACKET);
+        meta.n_missing_packets -= 1;
+      }
+    }
+  }
+
+  delete[] frame_buffer;
 }
