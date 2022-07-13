0368d8ad63f25c1eeb474c6bef9de2f025b800d4
Andrej Babic <andrej.babic@ringlspil.com>
2022-07-13 11:56:41 +0200

    Finalize the dump tool

--- std-udp-tools/src/std_udp_dump.cpp
+++ std-udp-tools/src/std_udp_dump.cpp
@@ -8,8 +8,11 @@
 #include "udp_recv_config.hpp"
 
 
+// Since jumbo frames are 9000, there should be no packet larger than that.
 const int UDP_BUFFER_MAX_SIZE = 1024 * 10;
+// 100ms timeout for stopping the program.
 const int BUFFER_UDP_US_TIMEOUT = 1000 * 100;
+// Buffer for each UDP connection - 20MB should be enough.
 const int BUFFER_UDP_RCVBUF_BYTES = 1024 * 1024 * 20;
 
 using namespace std;
@@ -53,12 +56,36 @@ void receive_and_dump(uint16_t udp_port, atomic_bool& run)
 {
   auto socket_fd = bind_udp_socket(udp_port);
   const auto file_name = fmt::format("{}.dat", udp_port);
+  ofstream file (file_name, ios::out | ios::binary);
 
   char* buffer = new char[UDP_BUFFER_MAX_SIZE];
+  size_t n_packets = 0;
+
+  fmt::print("[{}] Receiver dumping to {}\n", udp_port, file_name);
 
   while (run) {
+    const int n_recv_bytes = recv(socket_fd, buffer, UDP_BUFFER_MAX_SIZE, 0);
+    if (n_recv_bytes < 0) {
+      if (errno == EAGAIN) {
+        continue;
+      }
+      throw std::runtime_error(fmt::format("Cannot recv from socket, error: {}", errno));
+    }
 
+    if (n_recv_bytes == UDP_BUFFER_MAX_SIZE) {
+      throw std::runtime_error("Current udp buffer to small for received packet.");
+    }
+
+    n_packets += 1;
+    fmt::print("[{}] Received {} packets.\n", udp_port, n_packets);
+
+    const uint64_t header = n_recv_bytes;
+    file.write(reinterpret_cast<const char*>(&header), sizeof(header));
+    file.write(buffer, n_recv_bytes);
+    file.flush();
   }
+
+  file.close();
   delete[] buffer;
 }
 
@@ -68,7 +95,7 @@ int main(int argc, char** argv)
 
   if (argc != 2) {
     cout << endl;
-    cout << "Usage: std_udp_dump [detector_json_filename]";
+    cout << "Usage: std_udp_tools_dump [detector_json_filename]";
     cout << endl;
     cout << "\tdetector_json_filename: detector config file path." << endl;
     cout << endl;
@@ -80,13 +107,23 @@ int main(int argc, char** argv)
   std::vector<std::thread> threads;
   atomic_bool running(true);
 
+  fmt::print("Starting UDP dump for detector {} with n_modules {}\n",
+             config.detector_name, config.n_modules);
+
   for (int i=0; i<config.n_modules; i++) {
     auto udp_port = config.start_udp_port + i;
     threads.emplace_back(receive_and_dump, udp_port, std::ref(running));
   }
 
+  // Delay a bit so all threads report they started.
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  fmt::print("Press enter to stop the program...\n");
+
   cin.get();
   running = false;
+
+  fmt::print("Closing sockets.\n");
 
   for (auto& thread : threads) {
     thread.join();
