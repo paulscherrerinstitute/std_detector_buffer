0bca25a75fa8ae385e64df705b95965b730b7c24
Maciej Patro <maciejpatro@gmail.com>
2022-07-20 11:05:09 +0200

    Ensure shared memory cleanup is always handled

--- testing/test_jungfrau_workflow.py
+++ testing/test_jungfrau_workflow.py
@@ -3,6 +3,7 @@ import time
 import pytest
 
 from testing.execution_helpers import build_command, run_command_in_parallel
+from testing.fixtures import test_path, cleanup_jungfrau_shared_memory
 
 
 class JungfrauConfigUdp:
@@ -26,3 +27,11 @@ class JungfrauConfigConverter:
     meta_bytes_per_packet = JungfrauConfigUdp.meta_bytes_per_packet
     bytes_per_packet = meta_bytes_per_packet + data_bytes_per_packet
     buffer_size = bytes_per_packet * slots
+
+
+@pytest.mark.asyncio
+async def test_udp_receiver_with_converter(test_path, cleanup_jungfrau_shared_memory):
+    command = build_command('std_udp_recv_jf', test_path / 'jungfrau_detector.json', JungfrauConfigUdp.id)
+
+    with run_command_in_parallel(command):
+        time.sleep(1)
