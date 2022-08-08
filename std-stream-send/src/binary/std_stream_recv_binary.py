00c73133c1f10dfd4f5b8486a828af662b289933
Andrej Babic <andrej.babic@ringlspil.com>
2022-08-08 12:23:19 +0200

    Add stream recv binary Python client

---
+++ std-stream-send/src/binary/std_stream_recv_binary.py
@@ -0,0 +1,80 @@
+from ctypes import Structure, c_uint16, c_uint64
+
+import zmq
+import numpy as np
+
+
+class ImageMetadata(Structure):
+    _pack_ = 1
+    _fields_ = [("id", c_uint64),
+                ("height", c_uint64),
+                ("width", c_uint64),
+                ("dtype", c_uint16),
+                ("status", c_uint16),
+                ("source_id", c_uint16)]
+
+    dtype_mapping = {
+        1: 'uint8', 2: 'uint16', 4: 'uint32', 8: 'uint64',
+        11: 'int8', 12: 'int16', 14: 'int32', 18: 'int64',
+        22: 'float16', 24: 'float32', 28: 'float64'
+    }
+
+    status_mapping = {
+        0: 'good_image', 1: 'missing_packets', 2: 'id_missmatch'
+    }
+
+    def get_dtype_description(self):
+        return self.dtype_mapping[self.dtype]
+
+    def get_status_description(self):
+        return self.status_mapping[self.status]
+
+    def __str__(self):
+        return f"id: {self.id}; " \
+               f"height: {(self.height >> 1) << 1}; " \
+               f"width: {self.width}; " \
+               f"dtype: {self.dtype}; " \
+               f"status: {self.status}; " \
+               f"source_id: {self.source_id}; "
+
+
+class StdStreamRecvBinary(object):
+    def __init__(self, input_stream_address, recv_timeout):
+        self.input_stream_address = input_stream_address
+        self.recv_timeout = recv_timeout
+
+        self.ctx = None
+        self.receiver = None
+
+    def connect(self):
+        if self.ctx is not None:
+            raise RuntimeError("Socket already connected.")
+
+        self.ctx = zmq.Context()
+        self.receiver = self.ctx.socket(zmq.SUB)
+        self.receiver.connect(self.input_stream_address)
+        self.receiver.subscribe("")
+
+    def disconnect(self):
+        try:
+            self.receiver.disconnect()
+            self.ctx.term()
+        finally:
+            pass
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
+
+    @staticmethod
+    def _deserializer(multipart_bytes):
+        meta = ImageMetadata.from_buffer(multipart_bytes[0])
+        data = np.frombuffer(multipart_bytes[1], dtype=meta.get_dtype_description(), shape=(meta.height, meta.width))
+
+        return meta, data
+
+    def recv(self):
+        return self.receiver.recv_serialized(self._deserializer)
