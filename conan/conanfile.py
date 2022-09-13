f6fdefb91c1c69ebf6e79ed808db0a5a1dc44759
Maciej Patro <maciejpatro@gmail.com>
2022-09-13 15:15:56 +0200

    update gtest version?

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -5,7 +5,7 @@ class SBD(ConanFile):
     generators = "cmake_find_package"
     requires = [
         "zeromq/4.3.4",
-        "gtest/1.11.0",
+        "gtest/1.12.1",
         "rapidjson/cci.20211112",
         "lz4/1.9.3",
         "date/3.0.1",
