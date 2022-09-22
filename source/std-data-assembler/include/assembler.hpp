deea8774e4f843a3e84ff2b52c05380d4a08947b
Maciej Patro <maciejpatro@gmail.com>
2022-09-22 14:04:48 +0200

    Gigafrost image assembly - convert 12bit data to 16bit data - first draft

--- source/std-data-assembler/include/assembler.hpp
+++ source/std-data-assembler/include/assembler.hpp
@@ -5,4 +5,21 @@
 #ifndef STD_DETECTOR_BUFFER_ASSEMBLER_HPP
 #define STD_DETECTOR_BUFFER_ASSEMBLER_HPP
 
+#include <cstdint>
+#include <vector>
+#include <span>
+
+namespace sda {
+
+class Assembler
+{
+public:
+  explicit Assembler(std::size_t pixels_number);
+  std::span<uint16_t> convert(std::span<char> data);
+private:
+  std::vector<uint16_t> converted;
+};
+
+} // namespace sda
+
 #endif // STD_DETECTOR_BUFFER_ASSEMBLER_HPP
--- source/std-data-assembler/src/assembler.cpp
+++ source/std-data-assembler/src/assembler.cpp
@@ -1,3 +1,44 @@
 /////////////////////////////////////////////////////////////////////
 // Copyright (c) 2022 Paul Scherrer Institute. All rights reserved.
 /////////////////////////////////////////////////////////////////////
+
+#include "assembler.hpp"
+
+namespace sda {
+
+namespace {
+#pragma pack(1)
+struct conversion_handle
+{
+  uint16_t p1 : 12;
+  uint16_t p2 : 12;
+  uint16_t p3 : 12;
+  uint16_t p4 : 12;
+};
+#pragma pack()
+} // namespace
+
+Assembler::Assembler(std::size_t pixels_number)
+    : converted(pixels_number)
+{}
+
+std::span<uint16_t> Assembler::convert(std::span<char> input)
+{
+  // in theory the pixels number should be divisible by 4 - so there should be no problem with
+  // alignment of conversion_handle
+  const std::span<conversion_handle> conversion(reinterpret_cast<conversion_handle*>(input.data()),
+                                                input.size() / sizeof(conversion_handle));
+
+  for (auto i = 0u, j = 0u; i < conversion.size(); i++, j += 4) {
+    converted[j] = conversion[i].p1;
+    converted[j + 1] = conversion[i].p2;
+    converted[j + 2] = conversion[i].p3;
+    converted[j + 3] = conversion[i].p4;
+  }
+
+  if(const auto )
+
+  return converted;
+}
+
+} // namespace sda
