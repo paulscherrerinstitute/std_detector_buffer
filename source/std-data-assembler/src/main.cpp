728319977349eeb07a7c4e4436f12d78109fbdfd
Maciej Patro <maciejpatro@gmail.com>
2022-09-27 14:40:25 +0200

    Handle detector parameters in assembler

--- source/std-data-assembler/src/main.cpp
+++ source/std-data-assembler/src/main.cpp
@@ -2,7 +2,27 @@
 // Copyright (c) 2022 Paul Scherrer Institute. All rights reserved.
 /////////////////////////////////////////////////////////////////////
 
-int main(int , char**)
+#include <cstdlib>
+
+#include <fmt/core.h>
+
+#include "buffer_utils.hpp"
+
+namespace {
+void check_number_of_arguments(int argc)
 {
-  return 0;
+  if (argc != 2) {
+    fmt::print("Usage: std_data_assembler [detector_json_filename] \n\n"
+               "\tdetector_json_filename: detector config file path.\n");
+    exit(-1);
+  }
+}
+} // namespace
+
+int main(int argc, char* argv[])
+{
+  check_number_of_arguments(argc);
+
+  const auto config = buffer_utils::read_json_config(std::string(argv[1]));
+  return config.n_modules;
 }
