31670e6b0587808a1dd45aae0ba64cd3fccf29db
Andrej Babic <andrej.babic@ringlspil.com>
2022-10-17 12:24:44 +0200

    Increase library version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="std_buffer",
-      version="1.0.6",
+      version="1.0.7",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
