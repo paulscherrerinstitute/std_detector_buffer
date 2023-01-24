bcbc0f4570f159dca06584cc9709668fa41058ab
Andrej Babic <andrej.babic@ringlspil.com>
2023-01-24 18:03:33 +0100

    Increase buffer version

--- docker/build_buffer_base.sh
+++ docker/build_buffer_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME="paulscherrerinstitute/std_detector_buffer_base"
-VERSION=3.1.1
+VERSION=3.2.0
 
 docker build --no-cache -f buffer_base.Dockerfile -t ${NAME} .
 docker tag ${NAME} ${NAME}:${VERSION}
