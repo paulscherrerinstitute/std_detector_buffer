e913ffca5c846ecc31407c8069cfb23df84d6337
Maciej Patro <maciejpatro@gmail.com>
2023-03-17 16:16:34 +0100

    Eiger converter in theory supporting multiple modules - figure out halfes still

--- source/std-data-convert/std-data-convert-eg/include/converter.hpp
+++ source/std-data-convert/std-data-convert-eg/include/converter.hpp
@@ -20,14 +20,15 @@ public:
   void convert(std::span<char> input_data, std::span<char> output_buffer) const;
 
 private:
-  static int calculate_start_index(int module_id, std::size_t image_width, std::size_t bit_depth);
-  static int calculate_row_jump(int module_id, std::size_t image_width, std::size_t bit_depth);
+  static int calculate_start_index(const utils::DetectorConfig& config, int module_id);
+  static int calculate_row_jump(std::size_t image_width, std::size_t bit_depth);
+  static int calculate_row_jump_direction(const utils::DetectorConfig& config, int module_id);
 
   const std::size_t height;
   const std::size_t width;
   const std::size_t input_row_size;
-  const int start_index;
   const int row_jump;
+  const int start_index;
 };
 
 } // namespace eg::sdc
--- source/std-data-convert/std-data-convert-eg/src/converter.cpp
+++ source/std-data-convert/std-data-convert-eg/src/converter.cpp
@@ -4,6 +4,8 @@
 
 #include <cstring>
 
+#include <fmt/core.h>
+
 #include "converter.hpp"
 #include "detectors/eiger.hpp"
 
@@ -13,9 +15,21 @@ Converter::Converter(const utils::DetectorConfig& config, int module_id)
     : height(config.image_pixel_height)
     , width(config.image_pixel_width)
     , input_row_size(MODULE_X_SIZE * config.bit_depth / 8)
-    , start_index(calculate_start_index(module_id, config.image_pixel_width, config.bit_depth))
-    , row_jump(calculate_row_jump(0, config.image_pixel_width, config.bit_depth))
-{}
+    , row_jump(calculate_row_jump(config.image_pixel_width, config.bit_depth) *
+               calculate_row_jump_direction(config, module_id))
+    , start_index(calculate_start_index(config, module_id))
+{
+  if (auto diff = std::abs(utils::get_module_start_position(config, module_id).x -
+                           utils::get_module_end_position(config, module_id).x);
+      diff != MODULE_X_SIZE)
+    throw std::runtime_error(fmt::format("Eiger module width cannot be different than {} (set {})!",
+                                         MODULE_X_SIZE, diff));
+  if (auto diff = std::abs(utils::get_module_start_position(config, module_id).y -
+                           utils::get_module_end_position(config, module_id).y);
+      diff != MODULE_Y_SIZE)
+    throw std::runtime_error(fmt::format(
+        "Eiger module height cannot be different than {} (set {})!", MODULE_Y_SIZE, diff));
+}
 
 void Converter::convert(std::span<char> input, std::span<char> output_buffer) const
 {
@@ -26,14 +40,29 @@ void Converter::convert(std::span<char> input, std::span<char> output_buffer) co
   }
 }
 
-int Converter::calculate_start_index(int module_id, std::size_t image_width, std::size_t bit_depth)
+int Converter::calculate_start_index(const utils::DetectorConfig& config, int module_id)
 {
-  return module_id % 2 == 0 ? 0 : (image_width - MODULE_X_SIZE) * bit_depth / 8;
+  const auto start_position = utils::get_module_start_position(config, module_id);
+  const auto row_jump = calculate_row_jump(config.image_pixel_width, config.bit_depth);
+
+  if (calculate_row_jump_direction(config, module_id) > 0)
+    return start_position.y * row_jump + start_position.x * config.bit_depth / 8;
+  else {
+    const auto end_position = utils::get_module_end_position(config, module_id);
+    return start_position.y * row_jump + end_position.x * config.bit_depth / 8;
+  }
 }
 
-int Converter::calculate_row_jump(int, std::size_t image_width, std::size_t bit_depth)
+int Converter::calculate_row_jump(std::size_t image_width, std::size_t bit_depth)
 {
   return image_width * bit_depth / 8;
+}
+
+int Converter::calculate_row_jump_direction(const utils::DetectorConfig& config, int module_id)
+{
+  const auto start_position = utils::get_module_start_position(config, module_id);
+  const auto end_position = utils::get_module_end_position(config, module_id);
+  return end_position.y < start_position.y ? -1 : 1;
 }
 
 } // namespace eg::sdc
