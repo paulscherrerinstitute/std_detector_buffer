a69edbb4bfa0b4a36cef5d9387c96df5782b1426
Maciej Patro <maciejpatro@gmail.com>
2023-03-22 12:20:06 +0100

    Converter adapted to current understanding of eiger conversion

--- source/std-data-convert/std-data-convert-eg/include/converter.hpp
+++ source/std-data-convert/std-data-convert-eg/include/converter.hpp
@@ -20,13 +20,12 @@ public:
   void convert(std::span<char> input_data, std::span<char> output_buffer) const;
 
 private:
+  static std::size_t calculate_row_jump(std::size_t image_width, std::size_t bit_depth);
   static int calculate_start_index(const utils::DetectorConfig& config, int module_id);
-  static int calculate_row_jump(std::size_t image_width, std::size_t bit_depth);
   static int calculate_row_jump_direction(const utils::DetectorConfig& config, int module_id);
 
-  const std::size_t height;
-  const std::size_t width;
-  const std::size_t input_row_size;
+  const std::size_t input_row_size_per_chip;
+  const std::size_t gap_size;
   const int row_jump;
   const int start_index;
 };
--- source/std-data-convert/std-data-convert-eg/src/converter.cpp
+++ source/std-data-convert/std-data-convert-eg/src/converter.cpp
@@ -12,10 +12,9 @@
 namespace eg::sdc {
 
 Converter::Converter(const utils::DetectorConfig& config, int module_id)
-    : height(config.image_pixel_height)
-    , width(config.image_pixel_width)
-    , input_row_size(MODULE_X_SIZE * config.bit_depth / 8)
-    , row_jump(calculate_row_jump(config.image_pixel_width, config.bit_depth) *
+    : input_row_size_per_chip(MODULE_X_SIZE * config.bit_depth / 16)
+    , gap_size(2 * config.bit_depth / 8)
+    , row_jump(static_cast<int>(calculate_row_jump(config.image_pixel_width, config.bit_depth)) *
                calculate_row_jump_direction(config, module_id))
     , start_index(calculate_start_index(config, module_id))
 {
@@ -33,10 +32,16 @@ Converter::Converter(const utils::DetectorConfig& config, int module_id)
 
 void Converter::convert(std::span<char> input, std::span<char> output_buffer) const
 {
+  const auto input_second_chip_pos = input_row_size_per_chip;
+  const auto output_second_chip_pos = input_second_chip_pos + gap_size;
+
   for (auto row = 0u; row < MODULE_Y_SIZE; row++) {
-    const auto input_start = row * input_row_size;
+    const auto input_start = row * input_row_size_per_chip;
     const auto output_start = start_index + row * row_jump;
-    std::memcpy(output_buffer.data() + output_start, input.data() + input_start, input_row_size);
+    std::memcpy(output_buffer.data() + output_start, input.data() + input_start,
+                input_row_size_per_chip);
+    std::memcpy(output_buffer.data() + output_start + output_second_chip_pos,
+                input.data() + input_start + input_second_chip_pos, input_row_size_per_chip);
   }
 }
 
@@ -44,16 +49,10 @@ int Converter::calculate_start_index(const utils::DetectorConfig& config, int mo
 {
   const auto start_position = utils::get_module_start_position(config, module_id);
   const auto row_jump = calculate_row_jump(config.image_pixel_width, config.bit_depth);
-
-  if (calculate_row_jump_direction(config, module_id) > 0)
-    return start_position.y * row_jump + start_position.x * config.bit_depth / 8;
-  else {
-    const auto end_position = utils::get_module_end_position(config, module_id);
-    return start_position.y * row_jump + end_position.x * config.bit_depth / 8;
-  }
+  return static_cast<int>(start_position.y * row_jump + start_position.x * config.bit_depth / 8);
 }
 
-int Converter::calculate_row_jump(std::size_t image_width, std::size_t bit_depth)
+std::size_t Converter::calculate_row_jump(std::size_t image_width, std::size_t bit_depth)
 {
   return image_width * bit_depth / 8;
 }
