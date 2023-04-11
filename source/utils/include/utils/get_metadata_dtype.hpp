5fcfe483fe3c0690d8b0c27d46b74e2a776dc84d
Maciej Patro <maciejpatro@gmail.com>
2023-04-11 16:17:05 +0200

    Update live stream to support changing size/type of the images for pco cameras

--- source/utils/include/utils/get_metadata_dtype.hpp
+++ source/utils/include/utils/get_metadata_dtype.hpp
@@ -20,6 +20,24 @@ std_daq_protocol::ImageMetadataDtype get_metadata_dtype(const DetectorConfig& co
   throw std::runtime_error("Unsupported bit_depth of the image");
 }
 
+std::size_t get_bytes_from_metadata_dtype(std_daq_protocol::ImageMetadataDtype type)
+{
+  if (type == std_daq_protocol::ImageMetadataDtype::float32) return 4;
+  if (type == std_daq_protocol::ImageMetadataDtype::uint8) return 1;
+  if (type == std_daq_protocol::ImageMetadataDtype::uint16) return 2;
+  if (type == std_daq_protocol::ImageMetadataDtype::uint32) return 4;
+  throw std::runtime_error("Unsupported bit_depth of the image");
+}
+
+std::string get_array10_type(std_daq_protocol::ImageMetadataDtype type)
+{
+  if (type == std_daq_protocol::ImageMetadataDtype::float32) return "float32";
+  if (type == std_daq_protocol::ImageMetadataDtype::uint8) return "uint8";
+  if (type == std_daq_protocol::ImageMetadataDtype::uint16) return "uint16";
+  if (type == std_daq_protocol::ImageMetadataDtype::uint32) return "uint32";
+  throw std::runtime_error("Unsupported type of the image");
+}
+
 } // namespace utils
 
 #endif // STD_DETECTOR_BUFFER_GET_METADATA_DTYPE_HPP
