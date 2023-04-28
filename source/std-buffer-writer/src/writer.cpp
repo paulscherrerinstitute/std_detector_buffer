3676e52bbe7a110a80816f0f8fd51c878774c76b
Maciej Patro <maciejpatro@gmail.com>
2023-04-28 12:43:09 +0200

    Root path for buffer writer is now configurable

--- source/std-buffer-writer/src/writer.cpp
+++ source/std-buffer-writer/src/writer.cpp
@@ -28,32 +28,34 @@ auto calculate_size(const std_daq_protocol::ImageMetadata* meta)
 
 } // namespace
 
-std::tuple<utils::DetectorConfig, std::string> read_arguments(int argc, char* argv[])
+std::tuple<utils::DetectorConfig, std::string, std::string> read_arguments(int argc, char* argv[])
 {
   auto program = utils::create_parser("std_buffer_writer");
-  program.add_argument("db_address").help("Address of Redis API compatible database to connect");
+  program.add_argument("--root_dir").help("Root directory where files will be stored").required();
+  program.add_argument("--db_address")
+      .help("Address of Redis API compatible database to connect")
+      .required();
   program = utils::parse_arguments(program, argc, argv);
 
   return {utils::read_config_from_json_file(program.get("detector_json_filename")),
-          program.get("db_address")};
+          program.get("--db_address"), program.get("--root_dir")};
 }
 
 int main(int argc, char* argv[])
 {
-  const auto [config, db_address] = read_arguments(argc, argv);
-  const auto sync_name = fmt::format("{}-image", config.detector_name);
-  const std::size_t max_data_bytes =
-      config.image_pixel_width * config.image_pixel_height * config.bit_depth / 8u;
+  const auto [config, db_address, root_dir] = read_arguments(argc, argv);
+  sbw::RedisSender sender(config.detector_name, db_address);
+  sbw::BufferWriter writer(root_dir + config.detector_name);
 
   auto ctx = zmq_ctx_new();
   zmq_ctx_set(ctx, ZMQ_IO_THREADS, zmq_io_threads);
 
+  const auto sync_name = fmt::format("{}-image", config.detector_name);
+  const std::size_t max_data_bytes =
+      config.image_pixel_width * config.image_pixel_height * config.bit_depth / 8u;
+
   auto receiver = cb::Communicator{{sync_name, max_data_bytes, buffer_config::RAM_BUFFER_N_SLOTS},
                                    {sync_name, ctx, cb::CONN_TYPE_CONNECT, ZMQ_SUB}};
-
-  sbw::RedisSender sender(config.detector_name, db_address);
-  // TODO this should be configurable
-  sbw::BufferWriter writer("/tmp/testing/" + config.detector_name);
 
   char buffer[512];
   std_daq_protocol::BufferedMetadata buffered_meta;
