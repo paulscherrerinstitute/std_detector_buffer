4faaac6daabda93de3d8ecc7393f0781344d241e
Maciej Patro <maciejpatro@gmail.com>
2023-05-12 10:37:11 +0200

    Unit testing the synchronizer

---
+++ source/std-data-sync/std-data-sync-module/test/test_synchronizer.cpp
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 Paul Scherrer Institute. All rights reserved.
+/////////////////////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+#include "synchronizer.hpp"
+
+struct SynchronizerWithoutModuleMapTest : public ::testing::Test
+{
+  static const auto modules = 2u;
+  static const auto queue_size = 3u;
+  Synchronizer sync{modules, queue_size};
+};
+
+TEST_F(SynchronizerWithoutModuleMapTest, ShouldSyncImageWhenAllModulesAreReceived)
+{
+  const auto image_id = 1;
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 0}));
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 1}));
+  EXPECT_EQ(image_id, sync.pop_next_full_image().image_id);
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+}
+
+TEST_F(SynchronizerWithoutModuleMapTest, ShouldDropImageWhenTwiceReceivingSameModule)
+{
+  const auto image_id = 33;
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 0}));
+  EXPECT_EQ(1, sync.process_image_metadata({image_id, 0, 0}));
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+}
+
+TEST_F(SynchronizerWithoutModuleMapTest, ShouldDropImageWhenQueueIsFull)
+{
+  for (auto id = 0u; id < queue_size; id++)
+    EXPECT_EQ(0, sync.process_image_metadata({id, 0, 0}));
+
+  EXPECT_EQ(1, sync.process_image_metadata({42, 0, 0}));
+}
+
+TEST_F(SynchronizerWithoutModuleMapTest, ShouldReturnImagesAlwaysInOrder)
+{
+  for (auto id = 0u; id < queue_size; id++)
+    EXPECT_EQ(0, sync.process_image_metadata({id, 0, 0}));
+
+  EXPECT_EQ(0, sync.process_image_metadata({2, 0, 1}));
+  EXPECT_EQ(0, sync.process_image_metadata({1, 0, 1}));
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+
+  EXPECT_EQ(0, sync.process_image_metadata({0, 0, 1}));
+  for (auto id = 0u; id < queue_size; id++)
+    EXPECT_EQ(id, sync.pop_next_full_image().image_id);
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+}
+
+struct SynchronizerWithModuleMapTest : public ::testing::Test
+{
+  static const auto modules = 8u;
+  static const auto queue_size = 3u;
+  Synchronizer sync{modules, queue_size, std::bitset<128>("101101")};
+};
+
+TEST_F(SynchronizerWithModuleMapTest, ShouldSyncImageWhenAllModulesAreReceived)
+{
+  const auto image_id = 1;
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 0}));
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 2}));
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 3}));
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+
+  EXPECT_EQ(0, sync.process_image_metadata({image_id, 0, 5}));
+  EXPECT_EQ(image_id, sync.pop_next_full_image().image_id);
+  EXPECT_EQ(INVALID_IMAGE_ID, sync.pop_next_full_image().image_id);
+}
