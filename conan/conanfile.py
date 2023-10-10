73172d4075d0cfb9a8041bd1cd490034db33eeaa
Maciej Patro <maciejpatro@gmail.com>
2023-10-10 08:56:38 +0200

    Update fmt version for consistency with spdlog

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -9,7 +9,7 @@ class SBD(ConanFile):
         "nlohmann_json/3.11.2",
         "lz4/1.9.4",
         "range-v3/0.12.0",
-        "fmt/10.0.0",
+        "fmt/10.1.1",
         "benchmark/1.6.1",
         "protobuf/3.21.12",
         "argparse/2.9",
