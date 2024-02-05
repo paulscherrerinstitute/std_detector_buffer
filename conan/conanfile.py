d77b7230d7bb797de045c78f2e740d64d519b8eb
Maciej Patro <maciejpatro@gmail.com>
2024-02-05 15:09:43 +0100

    Upgrade the dependencies the newer versions

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -4,22 +4,21 @@ from conans import ConanFile
 class SBD(ConanFile):
     generators = "cmake_find_package"
     requires = [
-        "zeromq/4.3.4",
-        "gtest/1.13.0",
-        "nlohmann_json/3.11.2",
+        "zeromq/4.3.5",
+        "gtest/1.14.0",
+        "nlohmann_json/3.11.3",
         "lz4/1.9.4",
         "range-v3/0.12.0",
-        "fmt/10.1.1",
-        "benchmark/1.6.1",
+        "fmt/10.2.1",
+        "benchmark/1.8.3",
         "protobuf/3.21.12",
         "argparse/3.0",
         "bsread_receiver/0.16.1@patro_m/psi",
-        "redis-plus-plus/1.3.7",
-        "c-blosc2/2.10.0",
-        "spdlog/1.12.0",
-        "hdf5/1.14.1",
-        "hash-library/8.0",
-        "zlib/1.2.13"
+        "redis-plus-plus/1.3.11",
+        "c-blosc2/2.13.1",
+        "spdlog/1.13.0",
+        "hdf5/1.14.3",
+        "hash-library/8.0"
     ]
 
     default_options = {"date:header_only": True,
