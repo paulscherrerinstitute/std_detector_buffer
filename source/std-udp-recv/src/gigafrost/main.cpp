cdf67f81c40df2acfce87d9962778d63f37951ea
Maciej Patro <maciejpatro@gmail.com>
2024-03-22 10:06:41 +0100

    preparation for testing udp receiver

--- source/std-udp-recv/src/gigafrost/main.cpp
+++ source/std-udp-recv/src/gigafrost/main.cpp
@@ -26,12 +26,12 @@ using namespace gf;
 // Initialize new frame metadata from first seen packet.
 inline void init_frame_metadata(const uint32_t module_size_x,
                                 const uint32_t module_size_y,
-                                const size_t FRAME_N_PACKETS,
+                                const size_t frame_n_packets,
                                 const GFUdpPacket& packet,
                                 GFFrame& meta)
 {
   meta.common.image_id = static_cast<uint64_t>(packet.frame_index);
-  meta.common.n_missing_packets = FRAME_N_PACKETS;
+  meta.common.n_missing_packets = frame_n_packets;
 
   meta.scan_id = packet.scan_id;
   meta.size_x = module_size_x;
@@ -60,6 +60,9 @@ inline void send_image_id(GFFrame& meta,
   stats.process(meta.common.n_missing_packets);
   // Invalidate the current buffer - we already send data out for this one.
   meta.common.image_id = INVALID_IMAGE_ID;
+
+  spdlog::debug("sending image_id={}, n_missing_packets={}", meta.common.image_id,
+                meta.common.n_missing_packets);
 }
 
 inline void process_packet(GFFrame& meta,
@@ -68,19 +71,18 @@ inline void process_packet(GFFrame& meta,
                            const size_t frame_buffer_offset,
                            cb::Communicator& sender,
                            FrameStatsCollector& stats,
-                           size_t PACKET_N_DATA_BYTES,
-                           size_t LAST_PACKET_N_DATA_BYTES,
-                           const size_t LAST_PACKET_STARTING_ROW)
+                           size_t bytes_per_packet,
+                           size_t bytes_of_last_packet,
+                           const size_t last_packet_starting_row)
 {
   // Record we have received this packet.
   meta.common.n_missing_packets -= 1;
 
   // If not the last packet just accumulate data in frame_buffer.
-  if (packet.packet_starting_row != LAST_PACKET_STARTING_ROW) {
-    memcpy(frame_buffer + frame_buffer_offset, packet.data, PACKET_N_DATA_BYTES);
-  }
+  if (packet.packet_starting_row != last_packet_starting_row)
+    memcpy(frame_buffer + frame_buffer_offset, packet.data, bytes_per_packet);
   else {
-    memcpy(frame_buffer + frame_buffer_offset, packet.data, LAST_PACKET_N_DATA_BYTES);
+    memcpy(frame_buffer + frame_buffer_offset, packet.data, bytes_of_last_packet);
 
     // Copy frame_buffer to ram_buffer and send pulse_id over zmq if last packet in frame.
     send_image_id(meta, frame_buffer, sender, stats);
@@ -99,44 +101,44 @@ int main(int argc, char* argv[])
   utils::log::logger l{prog_name, detector_config.log_level};
   const auto module_id = program->get<uint16_t>("module_id");
 
-  const uint32_t MODULE_N_X_PIXEL = module_n_x_pixels(detector_config.image_pixel_width);
-  const uint32_t MODULE_N_Y_PIXEL = module_n_y_pixels(detector_config.image_pixel_height);
-  const auto MODULE_N_DATA_BYTES =
+  const uint32_t width_in_pixels = module_n_x_pixels(detector_config.image_pixel_width);
+  const uint32_t height_in_pixels = module_n_y_pixels(detector_config.image_pixel_height);
+  const auto bytes_per_module =
       module_n_data_bytes(detector_config.image_pixel_height, detector_config.image_pixel_width);
-  const uint32_t PACKET_N_ROWS =
+  const uint32_t rows_per_packet =
       n_rows_per_packet(detector_config.image_pixel_height, detector_config.image_pixel_width);
-  auto PACKET_N_DATA_BYTES = n_data_bytes_per_packet(detector_config.image_pixel_height,
-                                                     detector_config.image_pixel_width);
-  const size_t FRAME_N_PACKETS =
+  auto bytes_per_packet = n_data_bytes_per_packet(detector_config.image_pixel_height,
+                                                  detector_config.image_pixel_width);
+  const size_t packets_in_frame =
       n_packets_per_frame(detector_config.image_pixel_height, detector_config.image_pixel_width);
 
   // Calculate if the last packet has the same number of rows as the rest of the packets.
-  auto LAST_PACKET_N_ROWS = static_cast<size_t>(MODULE_N_Y_PIXEL % PACKET_N_ROWS);
+  auto last_packet_n_rows = static_cast<size_t>(height_in_pixels % rows_per_packet);
   // If there is no reminder the last packet has the same number of rows as others.
-  if (LAST_PACKET_N_ROWS == 0) {
-    LAST_PACKET_N_ROWS = PACKET_N_ROWS;
-  }
+  if (last_packet_n_rows == 0) last_packet_n_rows = rows_per_packet;
 
   // Number of data bytes in the last packet.
-  auto LAST_PACKET_N_DATA_BYTES = static_cast<size_t>(MODULE_N_X_PIXEL * LAST_PACKET_N_ROWS * 1.5);
-  if ((LAST_PACKET_N_ROWS % 2 == 1) && (MODULE_N_X_PIXEL % 48 != 0)) {
-    LAST_PACKET_N_DATA_BYTES += 36;
-  }
+  auto bytes_of_last_packet = static_cast<size_t>(width_in_pixels * last_packet_n_rows * 1.5);
+  if ((last_packet_n_rows % 2 == 1) && (width_in_pixels % 48 != 0)) bytes_of_last_packet += 36;
 
   // Get offset of last packet in frame to know when to commit frame.
-  const size_t LAST_PACKET_STARTING_ROW = MODULE_N_Y_PIXEL - LAST_PACKET_N_ROWS;
-  const size_t FRAME_N_BYTES =
-      (PACKET_N_DATA_BYTES * (FRAME_N_PACKETS - 1)) + LAST_PACKET_N_DATA_BYTES;
+  const size_t start_row_last_packet = height_in_pixels - last_packet_n_rows;
+  const size_t bytes_of_frame = (bytes_per_packet * (packets_in_frame - 1)) + bytes_of_last_packet;
+
+  spdlog::debug("bytes_per_module={}, bytes_per_packet={}, packets_in_frame={}, "
+                "bytes_of_last_packet={}, bytes_of_frame={}",
+                bytes_per_module, bytes_per_packet, packets_in_frame, bytes_of_last_packet,
+                bytes_of_frame);
 
   auto ctx = zmq_ctx_new();
   const auto source_name = fmt::format("{}-{}", detector_config.detector_name, module_id);
 
-  const cb::RamBufferConfig buffer_config = {source_name, FRAME_N_BYTES, RAM_BUFFER_N_SLOTS};
+  const cb::RamBufferConfig buffer_config = {source_name, bytes_of_frame, RAM_BUFFER_N_SLOTS};
   const cb::CommunicatorConfig comm_config = {source_name, ctx, cb::CONN_TYPE_BIND, ZMQ_PUB};
 
   cb::Communicator sender{buffer_config, comm_config};
   PacketUdpReceiver receiver(detector_config.start_udp_port + module_id, sizeof(GFUdpPacket),
-                             FRAME_N_PACKETS);
+                             packets_in_frame);
   FrameStatsCollector stats(detector_config.detector_name, detector_config.stats_collection_period,
                             module_id);
 
@@ -147,10 +149,8 @@ int main(int argc, char* argv[])
   meta.common.module_id = module_id % 8;
 
   // TODO: Make 64 a const somewhere or read it programmatically (cache line size)
-  char* frame_buffer = static_cast<char*>(aligned_alloc(64, MODULE_N_DATA_BYTES));
-  if (frame_buffer == nullptr) {
-    throw runtime_error("Cannot allocate frame_buffer.");
-  }
+  char* frame_buffer = static_cast<char*>(aligned_alloc(64, bytes_per_module));
+  if (frame_buffer == nullptr) throw runtime_error("Cannot allocate frame_buffer.");
 
   while (true) {
     // Load n_packets into the packet_buffer.
@@ -160,23 +160,22 @@ int main(int argc, char* argv[])
       const auto& packet = packet_buffer[i_packet];
 
       // Offset in bytes =  number of rows * row size in pixels * 1.5 (12bit pixels)
-      const size_t frame_buffer_offset = packet.packet_starting_row * MODULE_N_X_PIXEL * 1.5;
+      const size_t frame_buffer_offset = packet.packet_starting_row * width_in_pixels * 1.5;
 
       // Packet belongs to the frame we are currently processing.
-      if (meta.common.image_id == packet.frame_index) {
+      if (meta.common.image_id == packet.frame_index)
         process_packet(meta, packet, frame_buffer, frame_buffer_offset, sender, stats,
-                       PACKET_N_DATA_BYTES, LAST_PACKET_N_DATA_BYTES, LAST_PACKET_STARTING_ROW);
-      }
+                       bytes_per_packet, bytes_of_last_packet, start_row_last_packet);
+
       else {
         // The buffer was not flushed because the last packet from the previous frame was missing.
-        if (meta.common.image_id != INVALID_IMAGE_ID) {
+        if (meta.common.image_id != INVALID_IMAGE_ID)
           send_image_id(meta, frame_buffer, sender, stats);
-        }
 
-        init_frame_metadata(MODULE_N_X_PIXEL, MODULE_N_Y_PIXEL, FRAME_N_PACKETS, packet, meta);
+        init_frame_metadata(width_in_pixels, height_in_pixels, packets_in_frame, packet, meta);
 
         process_packet(meta, packet, frame_buffer, frame_buffer_offset, sender, stats,
-                       PACKET_N_DATA_BYTES, LAST_PACKET_N_DATA_BYTES, LAST_PACKET_STARTING_ROW);
+                       bytes_per_packet, bytes_of_last_packet, start_row_last_packet);
       }
     }
 
