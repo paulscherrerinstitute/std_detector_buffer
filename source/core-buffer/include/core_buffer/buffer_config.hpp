074bb967b28d2b59ad84d2876b96052f86117c71
Maciej Patro <maciejpatro@gmail.com>
2024-04-12 08:45:30 +0200

    Increase ZMQ buffer queues - in preparation for testing all sending/receiving modes

--- source/core-buffer/include/core_buffer/buffer_config.hpp
+++ source/core-buffer/include/core_buffer/buffer_config.hpp
@@ -17,9 +17,9 @@ const int BUFFER_UDP_RCVBUF_BYTES = (128 * BUFFER_UDP_RCVBUF_N_SLOTS * 8246);
 // Microseconds timeout for UDP recv.
 const int BUFFER_UDP_US_TIMEOUT = 2 * 1000;
 // HWM for live stream from buffer.
-const int BUFFER_ZMQ_SNDHWM = 100;
+const int BUFFER_ZMQ_SNDHWM = 50000;
 // HWM for live stream from buffer.
-const int BUFFER_ZMQ_RCVHWM = 100;
+const int BUFFER_ZMQ_RCVHWM = 50000;
 // IPC address of the live stream.
 const std::string IPC_URL_BASE = "ipc:///tmp/";
 // Number of image slots in ram buffer - 10 seconds should be enough
