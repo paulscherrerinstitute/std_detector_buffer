725485f0e1aa6b17d6056211e3562ebcbd8819ab
Maciej Patro <maciejpatro@gmail.com>
2024-04-26 14:07:51 +0200

    driver now has configurable port number, number of writers is now mandatory

--- source/std-det-driver/src/main.cpp
+++ source/std-det-driver/src/main.cpp
@@ -31,42 +31,45 @@ void do_accept(boost::asio::ip::tcp::acceptor& acceptor,
   });
 }
 
-std::tuple<utils::DetectorConfig, std::string, std::size_t> read_arguments(int argc, char* argv[])
+std::tuple<utils::DetectorConfig, std::string, std::size_t, std::size_t> read_arguments(
+    int argc, char* argv[])
 {
   auto program = utils::create_parser("std_det_driver");
   program->add_argument("-s", "--source_suffix")
       .help("suffix for ipc source for ram_buffer")
       .default_value("image"s);
   program->add_argument("-n", "--number_of_writers")
-      .default_value(12ul)
+      .required()
       .scan<'u', std::size_t>()
       .help("Number of parallel writers");
+  program->add_argument("-p", "--port")
+      .default_value(8080ul)
+      .scan<'u', std::size_t>()
+      .help("websocket listening port");
 
   program = utils::parse_arguments(std::move(program), argc, argv);
   return {utils::read_config_from_json_file(program->get("detector_json_filename")),
-          program->get("--source_suffix"), program->get<std::size_t>("--number_of_writers")};
+          program->get("--source_suffix"), program->get<std::size_t>("--number_of_writers"),
+          program->get<std::size_t>("--port")};
 }
 
 } // namespace
 
 int main(int argc, char* argv[])
 {
-  const auto [config, source_suffix, number_of_writers] = read_arguments(argc, argv);
+  const auto [config, source_suffix, number_of_writers, port] = read_arguments(argc, argv);
   [[maybe_unused]] utils::log::logger l{"std_det_driver", config.log_level};
 
+  boost::asio::io_context ioc{1};
   auto const address = boost::asio::ip::make_address("0.0.0.0");
-  auto const port = static_cast<unsigned short>(8080);
-  const auto source_name = fmt::format("{}-{}", config.detector_name, source_suffix);
+  tcp::acceptor acceptor{ioc, {address, static_cast<boost::asio::ip::port_type>(port)}};
 
   auto sm = std::make_shared<std_driver::state_manager>();
+  const auto source_name = fmt::format("{}-{}", config.detector_name, source_suffix);
   auto driver =
       std::make_shared<std_driver::writer_driver>(sm, source_name, config, number_of_writers);
 
-  boost::asio::io_context ioc{1};
-
-  tcp::acceptor acceptor{ioc, {address, port}};
   do_accept(acceptor, sm, driver);
-
   ioc.run();
 
   return 0;
