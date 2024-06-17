652b68f6ab03e20e84c9cfcd3b93da8ffaee4522
Maciej Patro <maciejpatro@gmail.com>
2024-06-17 11:59:00 +0200

    Sample filter for gigafrost introduced - based on the detector flag

---
+++ source/std-gf-filter/src/filter_stats_collection.hpp
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Paul Scherrer Institute. All rights reserved.
+/////////////////////////////////////////////////////////////////////
+
+#ifndef STD_DETECTOR_BUFFER_UTILS_FILTER_STATS_COLLECTOR_HPP
+#define STD_DETECTOR_BUFFER_UTILS_FILTER_STATS_COLLECTOR_HPP
+
+#include "utils/stats/timed_stats_collector.hpp"
+
+class FilterStatsCollector : public utils::stats::TimedStatsCollector
+{
+  using Parent = utils::stats::TimedStatsCollector;
+
+public:
+  explicit FilterStatsCollector(std::string_view detector_name, std::chrono::seconds period)
+      : utils::stats::TimedStatsCollector(detector_name, period)
+  {}
+
+  [[nodiscard]] std::string additional_message() override
+  {
+    auto outcome =
+        fmt::format("{},forwarded_images={}", Parent::additional_message(), forwarded_images);
+    reset_stats();
+    return outcome;
+  }
+
+  void forward() { forwarded_images++; }
+
+private:
+  void reset_stats() { forwarded_images = 0; }
+  unsigned forwarded_images = 0;
+};
+
+#endif // STD_DETECTOR_BUFFER_UTILS_FILTER_STATS_COLLECTOR_HPP
---
+++ source/std-gf-filter/src/main.cpp
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Paul Scherrer Institute. All rights reserved.
+/////////////////////////////////////////////////////////////////////
+
+#include <string>
+
+#include <zmq.h>
+
+#include "core_buffer/communicator.hpp"
+#include "core_buffer/ram_buffer.hpp"
+#include "core_buffer/buffer_utils.hpp"
+#include "std_buffer/image_metadata.pb.h"
+#include "utils/utils.hpp"
+
+#include "filter_stats_collection.hpp"
+
+using namespace std::string_literals;
+
+namespace {
+
+constexpr auto zmq_io_threads = 1;
+
+std::tuple<utils::DetectorConfig, std::string, bool> read_arguments(int argc, char* argv[])
+{
+  auto program = utils::create_parser("std_gf_filter");
+  program->add_argument("-s", "--source_suffix")
+      .help("suffix for ipc source for ram_buffer")
+      .default_value("image"s);
+  program->add_argument("-n", "--no_filter").help("forward all images").flag();
+
+  program = utils::parse_arguments(std::move(program), argc, argv);
+  return {utils::read_config_from_json_file(program->get("detector_json_filename")),
+          program->get("--source_suffix"), program->get<bool>("--no_filter")};
+}
+
+} // namespace
+
+int main(int argc, char* argv[])
+{
+  const auto [config, source_suffix, always_forward] = read_arguments(argc, argv);
+  [[maybe_unused]] utils::log::logger l{"std_gf_filter", config.log_level};
+
+  auto ctx = zmq_ctx_new();
+  zmq_ctx_set(ctx, ZMQ_IO_THREADS, zmq_io_threads);
+
+  const auto source_name = fmt::format("{}-{}", config.detector_name, source_suffix);
+  const auto stream_address = fmt::format("{}-filter", config.detector_name);
+
+  auto receiver = cb::Communicator{
+      {source_name, utils::converted_image_n_bytes(config), buffer_config::RAM_BUFFER_N_SLOTS},
+      {source_name, ctx, cb::CONN_TYPE_CONNECT, ZMQ_SUB}};
+  auto sender_socket = buffer_utils::bind_socket(ctx, stream_address, ZMQ_PUB);
+
+  char buffer[512];
+  std_daq_protocol::ImageMetadata meta;
+
+  FilterStatsCollector stats(config.detector_name, config.stats_collection_period);
+
+  while (true) {
+    if (auto n_bytes = receiver.receive_meta(buffer); n_bytes > 0) {
+      meta.ParseFromArray(buffer, n_bytes);
+      if (always_forward || meta.gf().store_image()) {
+        stats.forward();
+        zmq_send(sender_socket, buffer, n_bytes, 0);
+      }
+      stats.process();
+    }
+    stats.print_stats();
+  }
+  return 0;
+}
