8bef4308bc047156500c010c2461f90344e67e8e
Maciej Patro <maciej.patro@psi.ch>
2025-06-26 11:56:00 +0200

    test

--- source/std-buffer/std-buffer-reader/src/buffer_handler.cpp
+++ source/std-buffer/std-buffer-reader/src/buffer_handler.cpp
@@ -48,6 +48,7 @@ std::optional<std_daq_protocol::ImageMetadata> BufferHandler::get_image(uint64_t
   cv_.wait(lock,
            [this] { return !metadatas_.empty() || no_more_data_.load(std::memory_order_acquire); });
 
+  spdlog::debug("one more time, no_more_data={}", no_more_data_.load(std::memory_order_acquire));
   if (auto meta = try_pop_image(image_id)) return meta;
   return std::nullopt;
 }
